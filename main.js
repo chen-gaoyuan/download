40268b1da1fb4f4f19d95317164a77c200ba4177
coder <coder@cctv.com>
2025-01-10 10:48:58 +0800

    增加下载次数显示

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+require("dotenv/config");
 const Koa = require("koa");
 const KoaStatic = require("koa-static");
 const path = require("path");
@@ -62,28 +63,56 @@ const renderHtml = (url, files) => {
       } else {
         str += `<td>${getSize(file.stat.size)}</td>`;
       }
-      str += `<td>${file.des || "-"}</td>`;
+      if (process.env.COUNTER == "1") {
+        str += `<td>${file.count}</td>`;
+      }
+      if (process.env.DESCRIPTION == "1") {
+        str += `<td>${file.des || "-"}</td>`;
+      }
       str += `</tr>`;
       return str;
     })
     .join("\n");
-  return `<html>
-<head><title>Download</title></head>
-<body>
-<h1>Index of ${url}</h1>
-<table style="border-spacing:15px 0px;">
-<tbody>
-<tr></tr>
-<tr><th>Type</th><th>Name</th><th>Last modified</th><th>Size</th><th>Description</th></tr>
-<tr><th colspan="5"><hr></th></tr>
+  return `<html><head><title>Download</title></head>
+<body><h1>Index of ${url}</h1><table style="border-spacing:15px 0px;"><tbody>
+<tr>
+  <th>Type</th><th>Name</th><th>Last modified</th><th>Size</th>
+  ${process.env.COUNTER == "1" ? "<th>Count</th>" : ""}
+  ${process.env.DESCRIPTION == "1" ? "<th>Description</th>" : ""}
+</tr>
+<tr><th colspan="6"><hr></th></tr>
 ${bodyStr}
-<tr><th colspan="5"><hr></th></tr>
-</tbody>
-</table>
-</body>
-</html>`;
+<tr><th colspan="6"><hr></th></tr>
+</tbody></table></body></html>`;
 };
 const staticPath = path.join(__dirname, "static");
+if (process.env.COUNTER == "1") {
+  app.use(async (ctx, next) => {
+    await next();
+    if (ctx.url.endsWith("/")) {
+      return;
+    }
+    if (ctx.status != 200) {
+      return;
+    }
+    const filePath = path.join(staticPath, ctx.url);
+    const fileName = path.basename(filePath);
+    if (fileName.startsWith(".")) {
+      return;
+    }
+    const dirPath = path.dirname(filePath);
+    const countFilePath = path.join(dirPath, ".cnt." + fileName);
+    let count;
+    try {
+      count = parseInt(await fs.readFile(countFilePath, "utf-8"));
+    } catch {
+      count = 0;
+    }
+    count++;
+    await fs.writeFile(countFilePath, count.toString());
+  });
+}
+
 app.use(KoaStatic(staticPath, { hidden: true }));
 
 app.use(async (ctx) => {
@@ -111,15 +140,21 @@ app.use(async (ctx) => {
       }
       const filePath = path.join(dirPath, name);
       const stat = await fs.stat(filePath);
-      let des;
-      try {
-        const desPath = path.join(dirPath, "." + name + ".des");
-        des = await fs.readFile(desPath, "utf-8");
-      } catch {
-        des = "";
+      let count = 0;
+      if (process.env.COUNTER == "1") {
+        try {
+          const countPath = path.join(dirPath, ".cnt." + name);
+          count = parseInt(await fs.readFile(countPath, "utf-8"));
+        } catch {}
       }
-
-      files.push({ name, stat, des });
+      let des = "";
+      if (process.env.DESCRIPTION == "1") {
+        try {
+          const desPath = path.join(dirPath, ".des." + name);
+          des = await fs.readFile(desPath, "utf-8");
+        } catch {}
+      }
+      files.push({ name, stat, des, count });
     }
     files.sort(sortFiles);
     ctx.type = "text/html";
@@ -129,6 +164,7 @@ app.use(async (ctx) => {
   }
 });
 
-app.listen(10080, () => {
-  console.log("Server is running~");
+const port = process.env.PORT ? Number(process.env.PORT) : 80;
+app.listen(port, () => {
+  console.log(`Server is running on http://127.0.0.1:${port}`);
 });
